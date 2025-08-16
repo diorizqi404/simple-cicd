5259ed91331b0662d7a221e93aa66447151d9be1
diorizqi404 <rizqiii.dio@gmail.com>
2025-08-17 00:07:17 +0700

    chore: initialize package.json for simple-cicd-express with express dependency

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hellso World');
+});
+
+// Start server
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server berjalan di http://0.0.0.0:${port}`);
+});
